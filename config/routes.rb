6cb857d95663a597d99e9514fc60278228e3eced
Jeff Schneiderman <schneidj@gmail.com>
2011-11-29 12:44:59 +0200

    chap 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Sample2::Application.routes.draw do
-  get "users/new"
-
+ 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
   root :to => 'pages#home'
+  
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
